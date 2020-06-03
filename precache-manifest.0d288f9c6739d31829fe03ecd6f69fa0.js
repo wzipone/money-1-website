992daab8f35cb04f47134026248db6d3e5eb72ef
wzipone <wzipone76@gmail.com>
2020-06-03 21:08:16 +0800

    deploy

---
+++ precache-manifest.0d288f9c6739d31829fe03ecd6f69fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb6c7a0a4b051c1564e630265205232",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "6a56f738a350d101a890",
+    "url": "/money-1-website/static/css/main.03c3d140.chunk.css"
+  },
+  {
+    "revision": "3ace233c788319712218",
+    "url": "/money-1-website/static/js/2.4aa29f20.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-1-website/static/js/2.4aa29f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a56f738a350d101a890",
+    "url": "/money-1-website/static/js/main.8be7e234.chunk.js"
+  },
+  {
+    "revision": "4a97da6b1b364a398da2",
+    "url": "/money-1-website/static/js/runtime-main.650a7111.js"
+  }
+]);
